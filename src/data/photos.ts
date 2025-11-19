fa1c06766e16df6d0eed974a0b13d9a77154819c
Mariia <mcherkashina77@gmail.com>
2025-11-19 23:00:19 +0100

    Rename image files and update references in photos.ts
    Renamed image files in the public/images directory to a consistent 'imgN.jpg' naming scheme and updated their references in src/data/photos.ts to match the new filenames. This improves file organization and consistency.

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,30 +1,30 @@
 export const photos = [
   {
-    src: '/images/111.jpg',
+    src: '/images/img1.jpg',
     title: 'Image 1',
   },
   {
-    src: '/images/2.jpg',
+    src: '/images/img2.jpg',
     title: 'Image 2',
   },
   {
-    src: '/images/3.jpg',
+    src: '/images/img3.jpg',
     title: 'Image 3',
   },
   {
-    src: '/images/4.jpg',
+    src: '/images/img4.jpg',
     title: 'Image 4',
   },
   {
-    src: '/images/5.jpg',
+    src: '/images/img5.jpg',
     title: 'Image 5',
   },
   {
-    src: '/images/6.jpg',
+    src: '/images/img6.jpg',
     title: 'Image 6',
   },
   {
-    src: '/images/7.jpg',
+    src: '/images/img7.jpg',
     title: 'Image 7',
   },
 ];
